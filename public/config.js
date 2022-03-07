b2dd05c43bf9763c33623a5c865dd6625eac9674
Maurycy Zarzycki <maurycy@evidentlycube.com>
2022-03-07 20:48:02 +0100

    Final small change + V1.2.0 release

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 const SSSSSS_CONFIG = {
     version: "1.2.0",
-    date: 'TBD',
+    date: '2022-03-07',
     title: "SSSSSS %type% v%version% (%date%)",
     type: "Web",
     socketUrl: typeof window !== 'undefined' ? window.location : ''
