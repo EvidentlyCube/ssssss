b462df305902e3b683f27d74f00fed1d07e7ce12
Maurycy Zarzycki <maurycy@evidentlycube.com>
2022-03-13 14:01:06 +0100

    1.3.1 release

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 const SSSSSS_CONFIG = {
-    version: "1.3.0",
-    date: '2022-03-11',
+    version: "1.3.1",
+    date: '2022-03-13',
     title: "SSSSSS %type% v%version% (%date%)",
     type: "Web",
     socketUrl: typeof window !== 'undefined' ? window.location : ''
